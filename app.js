85d4a53f975327c04a537918bd09ea87aa04b35f
Divya Kumar <divya.kumar@learningmatemumbai.local>
2019-07-26 20:05:13 +0530

    v63

--- app.js
+++ app.js
@@ -1,19 +1,36 @@
 const http = require('http');
 
+
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 
+app.use(bodyParser.urlencoded({ extended: false }));
+
+/*
 app.use((req, res, next) => {
-    console.log('In the middleware!');
+    console.log('This always runs!');
     next(); // Allows the request to continue to the next middleware in line
 });
+*/
 
-app.use((req, res, next) => {
+
+app.use('/add-product', (req, res, next) => {
+    console.log('In another middleware!');
+    res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>');
+});
+
+app.use('/product', (req, res, next) => {
+    console.log(req.body);
+    res.redirect('/');
+});
+
+app.use('/', (req, res, next) => {
     console.log('In another middleware!');
     res.send('<h1>Hello from Express!</h1>');
 });
 
-const server = http.createServer(app);
+//const server = http.createServer(app);
 
-server.listen(3000);
+app.listen(3000);
